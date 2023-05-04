39ef05c525471086e2de5346b0285ca6e31f5433
ankit-120 <roy.ankit5008@gmail.com>
2023-05-04 09:50:20 +0530

    updated StudentController.java

--- src/main/java/com/hms/controller/StudentController.java
+++ src/main/java/com/hms/controller/StudentController.java
@@ -12,52 +12,52 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/hms")
+@RequestMapping("/hms/student")
 public class StudentController {
     @Autowired
     private StudentService studentService;
 
     //POST - to create a student
-    @PostMapping("/student")
-    public ResponseEntity<StudentDto> createStudent(@Valid @RequestBody StudentDto studentDto){
+    @PostMapping("/register")
+    public ResponseEntity<StudentDto> createStudent(@Valid @RequestBody StudentDto studentDto) {
         StudentDto student = this.studentService.createStudent(studentDto);
         return new ResponseEntity<>(student, HttpStatus.CREATED);
     }
 
     //PUT - to update student
-    @PutMapping("/student/{studentId}")
-    public ResponseEntity<StudentDto> updateStudent(@Valid @RequestBody StudentDto studentDto,@PathVariable int studentId){
+    @PutMapping("/update/{studentId}")
+    public ResponseEntity<StudentDto> updateStudent(@Valid @RequestBody StudentDto studentDto, @PathVariable int studentId) {
         StudentDto studentDto1 = this.studentService.updateStudent(studentDto, studentId);
-        return new ResponseEntity<>(studentDto1,HttpStatus.OK);
+        return new ResponseEntity<>(studentDto1, HttpStatus.OK);
     }
 
     //GET - to get student by id
-    @GetMapping("/student/{studentId}")
-    public ResponseEntity<StudentDto> getStudent(@Valid @PathVariable int studentId){
+    @GetMapping("/{studentId}")
+    public ResponseEntity<StudentDto> getStudent(@Valid @PathVariable int studentId) {
         StudentDto student = this.studentService.getStudent(studentId);
-        return new ResponseEntity<>(student,HttpStatus.OK);
+        return new ResponseEntity<>(student, HttpStatus.OK);
     }
 
-    //GET - to get all student
-    @GetMapping("/student")
-    public ResponseEntity<List<StudentDto>> getAllStudent(){
+    @GetMapping("/")
+    public ResponseEntity<List<StudentDto>> getAllStudent() {
         List<StudentDto> allStudent = this.studentService.getAllStudent();
-        return new ResponseEntity<>(allStudent,HttpStatus.OK);
+        return new ResponseEntity<>(allStudent, HttpStatus.OK);
     }
+    //GET - to get all student
 
     //DELETE - to delete a student
-    @DeleteMapping("/student/{studentId}")
-    public ResponseEntity<ApiResponse> deleteStudent(@Valid @PathVariable int studentId){
+    @DeleteMapping("/{studentId}")
+    public ResponseEntity<ApiResponse> deleteStudent(@Valid @PathVariable int studentId) {
         this.studentService.deleteStudent(studentId);
-        ApiResponse apiResponse = new ApiResponse("User Deleted Successfully",true);
-        return new ResponseEntity<>(apiResponse,HttpStatus.OK);
+        ApiResponse apiResponse = new ApiResponse("User Deleted Successfully", true);
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
 
     //GET - to get a student by first name
-    @GetMapping("/student/name/{firstName}")
-    public ResponseEntity<List<StudentDto>> getByFirstName(@PathVariable String firstName){
+    @GetMapping("/{firstName}")
+    public ResponseEntity<List<StudentDto>> getByFirstName(@PathVariable String firstName) {
         List<StudentDto> students = this.studentService.getByFirstName(firstName);
-        return new ResponseEntity<>(students,HttpStatus.OK);
+        return new ResponseEntity<>(students, HttpStatus.OK);
     }
 
 }
